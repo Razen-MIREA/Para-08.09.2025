0d87f32bbb19693ab6c84fa7ecaaa6a86052069b
Razen-MIREA <razen.al-nuveyni@mail.ru>
2025-09-08 15:46:35 +0300

    Update TasksC++.cpp

--- TasksC++.cpp
+++ TasksC++.cpp
@@ -6,14 +6,7 @@ int main() {
     std::string param1 = "Практика номер 0";
     
     // Заменяем символ '0' на '1'
-    // Используем метод replace с указанием позиции и длины
-    size_t pos = param1.find('0');
-    if (pos != std::string::npos) {
-        param1.replace(pos, 1, "1");
-    }
-    
-    // Альтернативный вариант с использованием std::replace
-    // std::replace(param1.begin(), param1.end(), '0', '1');
+    std::replace(param1.begin(), param1.end(), '0', '1');
     
     // Выводим результат
     std::cout << param1 << std::endl;
